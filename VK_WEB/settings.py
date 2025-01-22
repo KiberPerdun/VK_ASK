1e18556abdfb5e3be9fa33a60eaeb472b8651aaa
Syusyukin Anatoly Vladimirovich <syusyukinav@student.bmstu.ru>
2025-01-22 16:01:12 +0300

    update

--- VK_WEB/settings.py
+++ VK_WEB/settings.py
@@ -75,6 +75,7 @@ WSGI_APPLICATION = 'VK_WEB.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -85,15 +86,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
 '''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-'''
 
 
 # Password validation
